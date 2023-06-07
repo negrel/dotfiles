d0352a42eee6d873060333c3b1ea315841093e05
Alexandre Negrel <negrel.dev@protonmail.com>
2023-06-07 19:51:28 +0200

    add mjs colorscheme template for eww

--- hosts/matebook/cfg/users/anegrel/desktop/apps/eww/default.nix
+++ hosts/matebook/cfg/users/anegrel/desktop/apps/eww/default.nix
@@ -6,5 +6,6 @@
 
     home.file.".config/eww".source = ./config;
     gen-theme.templates."colors.scss".source = ./templates/colors.scss;
+    gen-theme.templates."colors.mjs".source = ./templates/colors.mjs;
   };
 }
