3c346aa66c88994fe0fb251a055bb82b1f62b78d
Alexandre Negrel <negrel.dev@protonmail.com>
2023-06-07 19:51:07 +0200

    update hyprland config

--- hosts/matebook/cfg/users/anegrel/desktop/wm/hyprland/config/hypr/start.d/00-swayidle.sh
+++ hosts/matebook/cfg/users/anegrel/desktop/wm/hyprland/config/hypr/start.d/00-swayidle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 nohup swayidle -w \
-  timeout 300 'swaylock' \
-  timeout 600 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
+  timeout 600 'hyprctl dispatch dpms off' \
+  resume 'hyprctl dispatch dpms on' \
   before-sleep 'swaylock' &> /dev/null &
 
