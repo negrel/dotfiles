19e6ec23e371441ad5035de50a588d2bb3276407
Alexandre Negrel <negrel.dev@protonmail.com>
2023-09-18 14:13:34 +0200

    remove hyprland & gnome

--- hosts/matebook/cfg/users/anegrel/desktop/wm/hyprland/config/hypr/start.d/00-swayidle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-nohup swayidle -w \
-  timeout 600 'hyprctl dispatch dpms off' \
-  resume 'hyprctl dispatch dpms on' \
-  before-sleep 'swaylock' &> /dev/null &
-
--- hosts/matebook/cfg/users/anegrel/desktop/wm/hyprland/config/hypr/start.d/10-setup-workspaces.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Setup applications
-hyprctl dispatch exec "[workspace 1] $BROWSER"
-hyprctl dispatch exec "[workspace 9] alacritty -e htop"
-hyprctl dispatch exec "[workspace 10] evolution"
