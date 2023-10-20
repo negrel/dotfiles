f2a12d0777d3885675e68728b3d48f596afb2788
Alexandre Negrel <negrel.dev@protonmail.com>
2023-10-20 10:48:43 +0200

    remove image preview from neovim's telescope

--- hosts/matebook/cfg/users/anegrel/dev/tools/neovim/config/lua/plugins/telescope.lua
+++ hosts/matebook/cfg/users/anegrel/dev/tools/neovim/config/lua/plugins/telescope.lua
@@ -11,32 +11,6 @@ require("telescope").setup {
 				["<A-d>"] = actions.preview_scrolling_up
 			},
 		},
-		preview = {
-			mime_hook = function(filepath, bufnr, opts)
-				---@diagnostic disable-next-line: redefined-local
-				local is_image = function(filepath)
-					local image_extensions = { 'png', 'jpg', 'jpeg', 'webp', 'avif' } -- Supported image formats
-					local split_path = vim.split(filepath:lower(), '.', { plain = true })
-					local extension = split_path[#split_path]
-					return vim.tbl_contains(image_extensions, extension)
-				end
-				if is_image(filepath) then
-					local term = vim.api.nvim_open_term(bufnr, {})
-					local function send_output(_, data, _)
-						for _, d in ipairs(data) do
-							vim.api.nvim_chan_send(term, d .. '\r\n')
-						end
-					end
-					vim.fn.jobstart(
-						{
-							'catimg', filepath -- Terminal image viewer command
-						},
-						{ on_stdout = send_output, stdout_buffered = true, pty = true })
-				else
-					require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
-				end
-			end
-		},
 	},
 	pickers = {
 		find_files = {
