76cd6a998b06be050458180b410a14a600544e32
Alexandre Negrel <negrel.dev@protonmail.com>
2023-11-10 18:16:55 +0100

    configure networkmanager so it ignore docker interfaces

--- hosts/matebook/cfg/networking.nix
+++ hosts/matebook/cfg/networking.nix
@@ -4,5 +4,9 @@
   # Pick only one of the below networking options.
   networking.networkmanager = {
     enable = true; # Easiest to use and most distros use this by default.
+    extraConfig = ''
+      [keyfile]
+      unmanaged-devices=interface-name:br-*;interface-name:docker0;interface-name:veth*
+    '';
   };
 }
