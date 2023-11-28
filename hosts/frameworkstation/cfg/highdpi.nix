b0722f935a05b8d7014efd6aa1b1f5966bbcb41d
Alexandre Negrel <negrel.dev@protonmail.com>
2023-11-28 18:23:29 +0100

    Revert "remove fractional scaling"
    This reverts commit f115dbdc33d819e062277aeeb38d70cecc4bbdec.

--- hosts/frameworkstation/cfg/highdpi.nix
+++ hosts/frameworkstation/cfg/highdpi.nix
@@ -1,9 +1,4 @@
 { ... }:
 {
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  # QT
-  environment.sessionVariables.QT_ENABLE_HIGHDPI_SCALING = "1";
-  environment.sessionVariables.QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-  environment.sessionVariables.QT_SCALE_FACTOR = "1";
 }
