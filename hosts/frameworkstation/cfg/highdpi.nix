f115dbdc33d819e062277aeeb38d70cecc4bbdec
Alexandre Negrel <negrel.dev@protonmail.com>
2023-11-28 17:02:05 +0100

    remove fractional scaling

--- hosts/frameworkstation/cfg/highdpi.nix
+++ hosts/frameworkstation/cfg/highdpi.nix
@@ -1,4 +1,9 @@
 { ... }:
 {
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # QT
+  environment.sessionVariables.QT_ENABLE_HIGHDPI_SCALING = "1";
+  environment.sessionVariables.QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+  environment.sessionVariables.QT_SCALE_FACTOR = "1";
 }
