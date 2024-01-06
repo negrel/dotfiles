ea5f1db794fef593e834ff1eb0ea59ad0c60728f
Alexandre Negrel <alexandre@negrel.dev>
2024-01-06 11:03:43 +0100

    set TERMINAL env var in .profile

--- hosts/frameworkstation/cfg/users/anegrel/desktop/apps/alacritty/default.nix
+++ hosts/frameworkstation/cfg/users/anegrel/desktop/apps/alacritty/default.nix
@@ -10,5 +10,9 @@
       text = builtins.readFile ./templates/config.yml;
       destination = ".config/alacritty/alacritty.yml";
     };
+
+    dot-profile.scripts."00-alacritty".text = ''
+      export TERMINAL="alacritty -e"
+    '';
   };
 }
