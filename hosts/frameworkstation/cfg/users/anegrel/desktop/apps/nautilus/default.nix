64d0ffe1c91e15babbc7ebb894feed84c7b87a8f
Alexandre Negrel <alexandre@negrel.dev>
2024-01-06 12:42:01 +0100

    fix terminal app opened with xterm by Nautilus

--- hosts/frameworkstation/cfg/users/anegrel/desktop/apps/nautilus/default.nix
+++ hosts/frameworkstation/cfg/users/anegrel/desktop/apps/nautilus/default.nix
@@ -5,6 +5,8 @@
   home-manager.users.anegrel = { ... }: {
     home.packages = with pkgs; [
       gnome.nautilus
+      # Exec Terminal=True .desktop file with the right terminal emulator
+      xdg-terminal-exec
     ];
   };
 }
