74453796c4246e037075af7ba0eddf23dc87abfb
Alexandre Negrel <alexandre@negrel.dev>
2024-02-06 19:09:30 +0100

    disable amd gpu scatter gather support

--- hosts/frameworkstation/hardware-configuration.nix
+++ hosts/frameworkstation/hardware-configuration.nix
@@ -20,7 +20,7 @@
 
   # Use latest zen kernel.
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernelParams = [ ];
+  boot.kernelParams = [ "amdgpu.sg_display=0" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ ];
 
   fileSystems."/" =
