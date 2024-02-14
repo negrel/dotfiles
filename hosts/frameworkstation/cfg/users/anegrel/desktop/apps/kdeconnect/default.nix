ed7cec8c087ef9d0c255df1816babcf3726ad863
Alexandre Negrel <alexandre@negrel.dev>
2024-02-14 19:39:45 +0100

    start kdeconnectd on login

--- hosts/frameworkstation/cfg/users/anegrel/desktop/apps/kdeconnect/default.nix
+++ hosts/frameworkstation/cfg/users/anegrel/desktop/apps/kdeconnect/default.nix
@@ -2,5 +2,12 @@
 
 {
   programs.kdeconnect.enable = true;
+  programs.kdeconnect.package = pkgs.kdeconnect;
+
+  home-manager.users.anegrel = {
+    dot-profile.scripts."99-kdeconnectd".text = ''
+      ${pkgs.kdeconnect}/libexec/kdeconnectd &
+    '';
+  };
 }
 
