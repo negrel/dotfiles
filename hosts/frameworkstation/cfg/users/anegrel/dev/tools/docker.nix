20c798d99159cc38c1053acbf91229d2485001ba
Alexandre Negrel <alexandre@negrel.dev>
2024-03-09 10:45:11 +0100

    enable docker daemon ipv6 support

--- hosts/frameworkstation/cfg/users/anegrel/dev/tools/docker.nix
+++ hosts/frameworkstation/cfg/users/anegrel/dev/tools/docker.nix
@@ -7,8 +7,15 @@
     };
   };
 
-  virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "btrfs";
+  virtualisation.docker = {
+    enable = true;
+    storageDriver = "btrfs";
+    daemon.settings = {
+      ipv6 = true;
+      experimental = true;
+      ip6tables = true;
+    };
+  };
 
   home-manager.users.anegrel = { ... }: {
     home.file.".docker/config.json".text = lib.my.readSecret "negrel.docker.config.json";
