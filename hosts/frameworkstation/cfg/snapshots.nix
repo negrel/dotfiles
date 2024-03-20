7efe37d72fd1d02e5b75cf701e9c10f9d2280f81
Alexandre Negrel <alexandre@negrel.dev>
2024-03-20 12:34:42 +0100

    update snapshot timer script to only keep 3 latests snapshots

--- hosts/frameworkstation/cfg/snapshots.nix
+++ hosts/frameworkstation/cfg/snapshots.nix
@@ -15,6 +15,10 @@
       set -euo pipefail
       ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot -r \
         /home "/snapshots/home_snapshot_$(${pkgs.coreutils}/bin/date --rfc-3339=date | tr '-' '_')"
+
+      # Keep only 3 latests snapshots.
+      ${pkgs.coreutils}/bin/ls /snapshots | ${pkgs.coreutils}/bin/head -n -3 \
+      | ${pkgs.findutils}/bin/xargs -I{} ${pkgs.btrfs-progs}/bin/btrfs subvolume delete /snapshots/{}
     '';
     serviceConfig = {
       Type = "oneshot";
