59b2c1e2dfdbaaee68d3d91c53f74947f1422048
Alexandre Negrel <alexandre@negrel.dev>
2025-01-07 14:34:34 +0100

    enable italic for gruvbox neovim theme

--- hosts/frameworkstation/cfg/users/anegrel/dev/tools/neovim/config/lua/plug.lua
+++ hosts/frameworkstation/cfg/users/anegrel/dev/tools/neovim/config/lua/plug.lua
@@ -133,6 +133,7 @@ require("packer").startup {
 			config = function()
 				vim.g.gruvbox_material_background = "hard"
 				vim.g.gruvbox_material_float_style = "dim"
+				vim.g.gruvbox_material_enable_italic = true
 				vim.cmd.colorscheme("gruvbox-material")
 			end,
 		}
