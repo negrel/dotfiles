d883b3b1688b95ad2e820fbaf03b1679d265f073
Alexandre Negrel <alexandre@negrel.dev>
2025-02-16 12:14:44 +0100

    add android tools

--- hosts/frameworkstation/cfg/users/anegrel/dev/android.nix
+++ hosts/frameworkstation/cfg/users/anegrel/dev/android.nix
@@ -7,6 +7,12 @@
     };
   };
 
+  home-manager.users.anegrel =
+    { ... }:
+    {
+      home.packages = with pkgs; [ android-tools ];
+    };
+
   # Allow non root users to use adb
   services.udev.packages = [
     pkgs.android-udev-rules
