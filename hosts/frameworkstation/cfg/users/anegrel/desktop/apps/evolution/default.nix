be52281436b78f44a6305ebdbc90d5ba8908bc79
Alexandre Negrel <alexandre@negrel.dev>
2025-02-17 13:50:03 +0100

    add evolution

--- hosts/frameworkstation/cfg/users/anegrel/desktop/apps/evolution/default.nix
+++ hosts/frameworkstation/cfg/users/anegrel/desktop/apps/evolution/default.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }:
 
 {
-  home-manager.users.anegrel = { ... }: {
-    home.packages = with pkgs; [
-      # evolution
-      # protonmail-bridge
-      # evolution-with-protonmail-bridge
-    ];
-  };
+  home-manager.users.anegrel =
+    { ... }:
+    {
+      home.packages = with pkgs; [
+        evolution
+        # protonmail-bridge
+        # evolution-with-protonmail-bridge
+      ];
+    };
 }
