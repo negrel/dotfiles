0cc0d00546dad405ee17a5e6f9be32bdd4ec91d2
Alexandre Negrel <alexandre@negrel.dev>
2026-03-12 13:42:08 +0100

    remove sumneko lua language server

--- hosts/frameworkstation/cfg/users/anegrel/dev/tools/neovim/default.nix
+++ hosts/frameworkstation/cfg/users/anegrel/dev/tools/neovim/default.nix
@@ -1,44 +1,45 @@
 { pkgs, ... }:
 
 {
-  home-manager.users.anegrel = { ... }: {
-    home.packages = with pkgs; [
-      # LSP that mason fails to install.
-      nil
-      nixpkgs-fmt # Nix LSP and formatter
-      sumneko-lua-language-server
+  home-manager.users.anegrel =
+    { ... }:
+    {
+      home.packages = with pkgs; [
+        # LSP that mason fails to install.
+        nil
+        nixpkgs-fmt # Nix LSP and formatter
 
-      # terminal image viewer for telescope preview
-      catimg
+        # terminal image viewer for telescope preview
+        catimg
 
-      # Required to build telescope-fzf extension.
-      cmake
-    ];
+        # Required to build telescope-fzf extension.
+        cmake
+      ];
 
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+      };
+
+      home.file.".config/nvim/".source = ./config;
+      zshrc.scripts."00-nvim".text = ''
+        export EDITOR=nvim
+        if [ -n "$NVIM" ]; then
+          export EDITOR=nano
+
+          nvim() {
+            ${pkgs.neovim}/bin/nvim --server $NVIM --remote-tab "$(realpath "''${1:-.}")"
+          }
+        else
+          nvim() {
+            local dir="$(realpath "''${1:-$PWD}")"
+            title "nvim $(basename "$dir")"
+            ${pkgs.neovim}/bin/nvim "$dir"
+          }
+        fi
+
+        alias vi=nvim
+        alias vim=vi
+      '';
     };
-
-    home.file.".config/nvim/".source = ./config;
-    zshrc.scripts."00-nvim".text = ''
-      export EDITOR=nvim
-      if [ -n "$NVIM" ]; then
-        export EDITOR=nano
-
-        nvim() {
-          ${pkgs.neovim}/bin/nvim --server $NVIM --remote-tab "$(realpath "''${1:-.}")"
-        }
-      else
-        nvim() {
-          local dir="$(realpath "''${1:-$PWD}")"
-          title "nvim $(basename "$dir")"
-          ${pkgs.neovim}/bin/nvim "$dir"
-        }
-      fi
-
-      alias vi=nvim
-      alias vim=vi
-    '';
-  };
 }
