d7b7ae8997f334586fa0d13fd5e2bf3a5ab04cdb
Alexandre Negrel <alexandre@negrel.dev>
2026-03-12 14:02:46 +0100

    fix vista fonts

--- hosts/frameworkstation/cfg/font.nix
+++ hosts/frameworkstation/cfg/font.nix
@@ -3,13 +3,21 @@
 {
   fonts = {
     fontDir.enable = true;
-    packages = with pkgs; [ nerd-fonts.jetbrains-mono corefonts vistafonts ];
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      corefonts
+      vista-fonts
+    ];
 
     fontconfig = {
       enable = true;
       defaultFonts = {
         monospace = [ "JetBrainsMono Nerd Font Mono" ];
-        emoji = [ "FontAwesome" "Material Design Icons" "Noto Color Emoji" ];
+        emoji = [
+          "FontAwesome"
+          "Material Design Icons"
+          "Noto Color Emoji"
+        ];
       };
     };
   };
